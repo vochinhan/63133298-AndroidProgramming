fd6d506a770d255ff004684b46a35bddf50cdfd2
vochinhan <112261999+vochinhan@users.noreply.github.com>
2024-03-25 17:24:36 +0700

    .

--- Cau2_BMICalculatorApp/app/src/main/java/ntu_63133298/cau2_bmicalculatorapp/MainActivity.java
+++ Cau2_BMICalculatorApp/app/src/main/java/ntu_63133298/cau2_bmicalculatorapp/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu_63133298.cau2_bmicalculatorapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtHeight, edtWeight, edtBMI;
+    Button btnCal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getViews();
+
+        btnCal.setOnClickListener(calculateBMI);
     }
+
+    public void getViews()
+    {
+        edtHeight = findViewById(R.id.edtHeight);
+        edtWeight = findViewById(R.id.edtWeight);
+        edtBMI = findViewById(R.id.edtBMI);
+        btnCal = findViewById(R.id.btnCal);
+    }
+
+    View.OnClickListener calculateBMI = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strHeight = edtHeight.getText().toString();
+            String strWeight = edtWeight.getText().toString();
+
+            float fHeight = Float.parseFloat(strHeight);
+            float fWeight = Float.parseFloat(strWeight);
+
+            
+        }
+    };
 }
